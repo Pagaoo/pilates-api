92b679f1c69c118cac01f72dea7607964489897e
Pagão <gabrielpaganinibarroso@hotmail.com>
2024-12-03 14:55:19 -0300

    created functions to save and get for professors to the service file

--- src/main/java/com/dev/pilates/repositories/ProfessorRepository.java
+++ src/main/java/com/dev/pilates/repositories/ProfessorRepository.java
@@ -1,9 +1,13 @@
 package com.dev.pilates.repositories;
 
+import com.dev.pilates.dtos.professor.ProfessorDTO;
 import com.dev.pilates.entities.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+    List<ProfessorDTO> findProfessorByUsername(String username);
 }
--- src/main/java/com/dev/pilates/services/ProfessorServices.java
+++ src/main/java/com/dev/pilates/services/ProfessorServices.java
@@ -1,8 +1,19 @@
 package com.dev.pilates.services;
 
+import com.dev.pilates.dtos.professor.ProfessorDTO;
+import com.dev.pilates.entities.Professor;
+import com.dev.pilates.entities.Roles;
 import com.dev.pilates.repositories.ProfessorRepository;
 import com.dev.pilates.repositories.RolesRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProfessorServices {
@@ -12,5 +23,49 @@ public class ProfessorServices {
     public ProfessorServices(ProfessorRepository professorRepository, RolesRepository rolesRepository) {
         this.professorRepository = professorRepository;
         this.rolesRepository = rolesRepository;
+    }
+
+    public List<ProfessorDTO> findAll() {
+        List<Professor> professorList = professorRepository.findAll();
+        List<ProfessorDTO> professorDTOList = professorList.stream().map(Professor::toProfessorDTO).collect(Collectors.toList());
+        return professorDTOList;
+    }
+
+    public ProfessorDTO findProfessorById(Long id) {
+        Professor professor = professorRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Professor não encontrado"));
+        return professor.toProfessorDTO();
+    }
+
+    public List<ProfessorDTO> findProfessorByName(String username) {
+        List<ProfessorDTO> professorDTOSList = professorRepository.findProfessorByUsername(username);
+
+        if (professorDTOSList.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        return professorDTOSList;
+    }
+
+    public ProfessorDTO save(@Valid ProfessorDTO professorDTO) {
+        try {
+            Roles role = rolesRepository.findById(professorDTO.role_id()).orElseThrow(() ->
+                    new EntityNotFoundException(String.format("Role de id: %s não encontrada", professorDTO.role_id())));
+            Professor newProfessor = convertToProfessorDTO(professorDTO, role);
+            professorRepository.save(newProfessor);
+            return newProfessor.toProfessorDTO();
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Erro de integridade ao criar professor", e.getCause());
+        }
+    }
+
+    private Professor convertToProfessorDTO(ProfessorDTO professorDTO, Roles roles) {
+        Professor professorRequest = new Professor();
+        professorRequest.setUsername(professorDTO.username());
+        professorRequest.setPassword(professorDTO.password());
+        professorRequest.setRole(roles);
+        professorRequest.setCreated_at(LocalDateTime.now());
+        professorRequest.setUpdated_at(LocalDateTime.now());
+        return professorRequest;
     }
 }
