c360df11c749f2ab3d5c4b6ac53a83d76f07c58b
Pagão <gabrielpaganinibarroso@hotmail.com>
2025-02-20 16:13:51 -0300

    fix: changed HttpNotReadableException to HttpMessageConversion

--- src/main/java/com/dev/pilates/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/dev/pilates/exceptions/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -45,8 +46,8 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableJsonException(HttpServletRequest request) {
+    @ExceptionHandler(HttpMessageConversionException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageConversionException(HttpServletRequest request) {
         String timestamp = DateTimeConfig.FORMATTER.format(Instant.now());
 
         ErrorResponse errorResponse = new ErrorResponse(
